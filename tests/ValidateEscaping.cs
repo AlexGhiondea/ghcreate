440f6ccd75d0cf3dd580062a68f9824639eb2a59
Alex Ghiondea <alex.ghiondea@gmail.com>
2021-03-05 17:21:42 -0800

    Add test for escaping newline.

--- tests/ValidateEscaping.cs
+++ tests/ValidateEscaping.cs
@@ -18,6 +18,9 @@ namespace Creator.Tests
         [TestCase("string with spaces", "string with spaces")]
         [TestCase("string\"", "\"string\"\"\"")]
         [TestCase(null, null)]
+        [TestCase(@"string
+with newline", @"""string
+with newline""")]
         public void ConvertMilestone(string input, string expected)
         {
             Assert.AreEqual(expected, StringHelpers.EncodeString(input));
